7f6e5217904b70169550247305da249215e9c649
Alessandro Delle Foglie <alessandro.delfo1@gmail.com>
2020-12-03 20:26:33 +0100

    New API to get all food

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 from flask import Flask
 from flask import jsonify
 import os
+import mysql.connector
 
 database_host = os.environ['MYSQL_HOST']
 database_username = os.environ['MYSQL_USERNAME']
 database_password = os.environ['MYSQL_PASS']
 
 app = Flask("Cinema Backend")
+
+
+@app.route('/api/food/getall', methods=['GET'])
+def get_all_food():
+    cnx = mysql.connector.connect(user=database_username, password=database_password,
+                                  host=database_host,
+                                  database='Cinema')
+    cursor = cnx.cursor()
+    cursor.execute("SELECT * FROM food ")
+    food_list = []
+    for (food_id, title, ingredients, image, price, calories, description) in cursor:
+        food_dict = dict()
+        food_dict['id'] = food_id
+        food_dict['title'] = title
+        food_dict['ingredients'] = ingredients
+        food_dict['image'] = image
+        food_dict['price'] = price
+        food_dict['calories'] = calories
+        food_dict['description'] = description
+        food_list.append(food_dict)
+
+    return jsonify(food_list), 200
 
 
 @app.route('/api/ping', methods=['GET'])
